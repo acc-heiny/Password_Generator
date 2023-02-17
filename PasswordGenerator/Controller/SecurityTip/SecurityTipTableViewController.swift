71d5e1c8cc6d45ddaf73af6f019c76c34eedf9e1
Hellen Heiny <hellen.heiny.f.santi@accenture.com>
2023-02-17 14:56:40 -0300

    Move  SecurityTipsLoader to  SecurityTipModel file

--- PasswordGenerator/Controller/SecurityTip/SecurityTipTableViewController.swift
+++ PasswordGenerator/Controller/SecurityTip/SecurityTipTableViewController.swift
@@ -37,18 +37,3 @@ class SecurityTipTableViewController: UITableViewController {
         dismiss(animated: true)
     }
 }
-
-struct SecurityTipsLoader {
-    var tips: [SecurityTipModel] = []
-    
-    mutating func loadTips() {
-        guard let fileURL: URL = Bundle.main.url(forResource: "DicasSenha.json", withExtension: nil),
-              let jsonData = try? Data(contentsOf: fileURL) else {return}
-        
-        do {
-            tips = try JSONDecoder().decode([SecurityTipModel].self, from: jsonData)
-        } catch {
-            print(error.localizedDescription)
-        }
-    }
-}
